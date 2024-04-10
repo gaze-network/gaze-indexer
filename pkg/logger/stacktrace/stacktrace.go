b923d8343226bea2763c2ff8df60ec1655ae7e5e
Gaze <dev@gaze.network>
2024-04-10 09:04:00 +0700

    feat(stacktrace): use runtime.Frame instead manual parse
    Co-authored-by: Gaze <gazenw@users.noreply.github.com>

--- pkg/logger/stacktrace/stacktrace.go
+++ pkg/logger/stacktrace/stacktrace.go
@@ -5,16 +5,14 @@ import (
 	"io"
 	"runtime"
 	"strings"
-
-	"github.com/cockroachdb/errors/errbase"
 )
-
-// The maximum number of stackframes to capture.
-var MaxStackDepth = 64
 
 // StackTrace is the type of the data for a call stack.
 // This mirrors the type of the same name in [github.com/cockroachdb/errors/errbase.StackTrace].
-type StackTrace errbase.StackTrace
+type StackTrace struct {
+	PCS    []uintptr
+	Frames []Frame
+}
 
 // Caller captures a stack trace of the specified depth, skipping
 // the provided number of frames. skip=0 identifies the caller of Caller.
@@ -27,50 +25,66 @@ func Caller(skip int) *StackTrace {
 // Capture captures a stack trace of the specified depth, skipping
 // the provided number of frames. skip=0 identifies the caller of Capture.
 func Capture(skip int) *StackTrace {
-	pcs := make([]uintptr, MaxStackDepth)
+	pcs := make([]uintptr, 64)
 	n := runtime.Callers(2+skip, pcs[:])
-	f := make([]errbase.StackFrame, n)
-	for i := 0; i < len(f); i++ {
-		f[i] = errbase.StackFrame(pcs[i])
+
+	// Expand the pcs slice if there wasn't enough room.
+	for n == len(pcs) {
+		pcs = make([]uintptr, 2*len(pcs))
+		n = runtime.Callers(2+skip, pcs[:])
 	}
-	return (*StackTrace)(&f)
+
+	// Deallocate the unused space in the slice.
+	pcs = pcs[:n:n]
+
+	// Preprocess stack frames.
+	frames := make([]Frame, 0, len(pcs))
+	callerFrames := runtime.CallersFrames(pcs)
+	for frame, more := callerFrames.Next(); more; frame, more = callerFrames.Next() {
+		if !strings.HasPrefix(frame.Function, "runtime.") {
+			frames = append(frames, Frame{frame})
+		}
+	}
+
+	return &StackTrace{
+		PCS:    pcs,
+		Frames: frames[:len(frames):len(frames)],
+	}
 }
 
-// TraceFrames returns the trace line frames of the stack trace.
-func (s *StackTrace) TraceFrames() []TraceFrame {
-	return TraceLines(s)
+// FramesStrings returns the frames of this stacktrace as slice of strings.
+func (s *StackTrace) FramesStrings() []string {
+	str := make([]string, len(s.Frames))
+	for i, frame := range s.Frames {
+		str[i] = frame.String()
+	}
+	return str
 }
 
-func (s *StackTrace) TraceFramesStrings() []string {
-	traceLines := s.TraceFrames()
-	t := make([]string, len(traceLines))
-	for i, tl := range traceLines {
-		t[i] = tl.String()
-	}
-	return t
+// Count reports the total number of frames in this stacktrace.
+func (s *StackTrace) Count() int {
+	return len(s.Frames)
 }
 
 // Format formats the stack of Frames according to the fmt.Formatter interface.
 func (s *StackTrace) Format(fs fmt.State, verb rune) {
-	tracelines := s.TraceFrames()
-	for i, tl := range tracelines {
+	for i, frame := range s.Frames {
 		if i > 0 {
 			_, _ = io.WriteString(fs, "\n")
 		}
-		tl.Format(fs, verb)
+		frame.Format(fs, verb)
 	}
 }
 
 // String returns a string representation of the stack trace.
 func (s *StackTrace) String() string {
 	var sb strings.Builder
-	tracelines := s.TraceFrames()
-	for i, tl := range tracelines {
+	for i, frame := range s.Frames {
 		if i > 0 {
 			sb.WriteString(", ")
 		}
-		sb.WriteString(fmt.Sprintf("[%d] ", i+1))
-		sb.WriteString(tl.String())
+		sb.WriteString(fmt.Sprintf("[%d] ", len(s.Frames)-i))
+		sb.WriteString(frame.String())
 	}
 	return sb.String()
 }
