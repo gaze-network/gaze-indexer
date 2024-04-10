3f03e542568b0a3f438abb3e0587b96415f72b28
Gaze <dev@gaze.network>
2024-04-10 09:20:01 +0700

    feat(logger): use stacktrace frame to log
    Co-authored-by: Gaze <gazenw@users.noreply.github.com>

--- pkg/logger/error.go
+++ pkg/logger/error.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
-	"runtime"
-	"strings"
 
-	"github.com/cockroachdb/errors/errbase"
 	"github.com/gaze-network/indexer-network/pkg/logger/slogx"
+	"github.com/gaze-network/indexer-network/pkg/stacktrace"
 )
 
 func middlewareError() middleware {
@@ -19,46 +17,14 @@ func middlewareError() middleware {
 					err := attr.Value.Any()
 					if err, ok := err.(error); ok && err != nil {
 						rec.AddAttrs(slog.String("error_verbose", fmt.Sprintf("%+v", err)))
-						if x, ok := err.(errbase.StackTraceProvider); ok {
-							rec.AddAttrs(slog.Any("stack_trace", traceLines(x.StackTrace())))
+						if st, ok := stacktrace.ParseErrStackTrace(err); ok {
+							rec.AddAttrs(slog.Any("stack_trace", st.FramesStrings()))
 						}
 					}
 				}
 				return false
 			})
-
 			return next(ctx, rec)
 		}
 	}
-}
-
-func traceLines(frames errbase.StackTrace) []string {
-	traceLines := make([]string, 0, len(frames))
-
-	// Iterate in reverse to skip uninteresting, consecutive runtime frames at
-	// the bottom of the trace.
-	skipping := true
-	for i := len(frames) - 1; i >= 0; i-- {
-		// Adapted from errors.Frame.MarshalText(), but avoiding repeated
-		// calls to FuncForPC and FileLine.
-		pc := uintptr(frames[i]) - 1
-		fn := runtime.FuncForPC(pc)
-		if fn == nil {
-			traceLines = append(traceLines, "unknown")
-			skipping = false
-			continue
-		}
-
-		name := fn.Name()
-		if skipping && strings.HasPrefix(name, "runtime.") {
-			continue
-		} else {
-			skipping = false
-		}
-
-		filename, lineNr := fn.FileLine(pc)
-		traceLines = append(traceLines, fmt.Sprintf("%s %s:%d", name, filename, lineNr))
-	}
-
-	return traceLines[:len(traceLines):len(traceLines)]
 }
