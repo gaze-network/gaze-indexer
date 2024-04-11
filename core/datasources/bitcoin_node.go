12a18d7bfdd562710ff2a2acec6f78a2d71562b2
Gaze <gazenw@users.noreply.github.com>
2024-04-11 20:44:21 +0700

    feat(btc): prepare bitcoin node datasource
    Co-authored-by: Gaze <dev@gaze.network>

--- core/datasources/bitcoin_node.go
+++ core/datasources/bitcoin_node.go
@@ -3,6 +3,7 @@ package datasources
 import (
 	"context"
 
+	"github.com/cockroachdb/errors"
 	"github.com/gaze-network/indexer-network/core/indexers"
 	"github.com/gaze-network/indexer-network/core/types"
 )
@@ -13,10 +14,28 @@ var _ indexers.BitcoinDatasource = (*BitcoinNodeDatasource)(nil)
 type BitcoinNodeDatasource struct{}
 
 func (d *BitcoinNodeDatasource) Fetch(ctx context.Context, from, to int64) ([]*types.Block, error) {
-	return nil, nil
+	ch, stop, err := d.FetchAsync(ctx, from, to)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	blocks := make([]*types.Block, 0)
+	for {
+		select {
+		case b, ok := <-ch:
+			if !ok {
+				return blocks, nil
+			}
+			blocks = append(blocks, b...)
+		case <-ctx.Done():
+			stop()
+			return nil, errors.Wrap(ctx.Err(), "context done")
+		}
+	}
 }
 
-func (d *BitcoinNodeDatasource) FetchAsync(ctx context.Context, from, to int64) (chan<- []*types.Block, func(), error) {
+func (d *BitcoinNodeDatasource) FetchAsync(ctx context.Context, from, to int64) (<-chan []*types.Block, func(), error) {
 	ctx, cancel := context.WithCancel(ctx)
+	_ = ctx
 	return nil, cancel, nil
 }
