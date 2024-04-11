7be2fdba83896ac4198195085843c615c4c38215
Gaze <gazenw@users.noreply.github.com>
2024-04-11 20:12:16 +0700

    feat(indexer): update indexer interface
    Co-authored-by: Gaze <dev@gaze.network>

--- core/indexers/bitcoin_indexer.go
+++ core/indexers/bitcoin_indexer.go
@@ -13,27 +13,15 @@ import (
 	"github.com/gaze-network/indexer-network/pkg/logger"
 )
 
-// BitcoinProcessor is indexer processor for Bitcoin Indexer.
-type BitcoinProcessor interface {
-	Processor
-
-	// Process processes the input data and indexes it.
-	Process(ctx context.Context, inputs []*types.Block) error
-
-	// CurrentBlock returns the latest indexed block header.
-	CurrentBlock(ctx context.Context) (types.BlockHeader, error)
-
-	// PrepareData fetches the data from the source and prepares it for processing.
-	// TODO: extract PrepareData to a separate interface (e.g. DataFetcher)
-	PrepareData(ctx context.Context, from, to int64) ([]*types.Block, error)
-
-	// RevertData revert synced data to the specified block for re-indexing.
-	RevertData(ctx context.Context, from types.BlockHeader) error
-}
+type (
+	BitcoinProcessor  Processor[*types.Block]
+	BitcoinDatasource Datasource[*types.Block]
+)
 
 // BitcoinIndexer is the indexer for sync Bitcoin data to the database.
 type BitcoinIndexer struct {
 	Processor    BitcoinProcessor
+	Datasource   BitcoinDatasource
 	btcclient    *rpcclient.Client
 	currentBlock types.BlockHeader
 }
