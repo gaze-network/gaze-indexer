303a6d7cec2e3d675cbea54874abdf7c28c656c0
Gaze <gazenw@users.noreply.github.com>
2024-04-13 22:03:18 +0700

    feat(btc): update indexer flow
    Co-authored-by: Gaze <dev@gaze.network>

--- core/indexers/bitcoin_indexer.go
+++ core/indexers/bitcoin_indexer.go
@@ -2,7 +2,6 @@ package indexers
 
 import (
 	"context"
-	"log"
 	"log/slog"
 	"time"
 
@@ -49,78 +48,44 @@ func (i *BitcoinIndexer) Run(ctx context.Context) (err error) {
 		case <-ctx.Done():
 			return nil
 		case <-ticker.C:
-			ch := make(chan []*types.Block)
-			subscription, err := i.Datasource.FetchAsync(ctx, i.currentBlock.Height-1, -1, ch)
-			if err != nil {
-				return errors.Wrap(err, "failed to call fetch async")
+			if err := i.process(ctx); err != nil {
+				return errors.Wrap(err, "failed to process")
 			}
+		}
+	}
+}
 
-			for {
-				select {
-				case blocks := <-ch:
-					// empty blocks
-					if len(blocks) == 0 {
-						continue
-					}
+func (i *BitcoinIndexer) process(ctx context.Context) (err error) {
+	ch := make(chan []*types.Block)
+	subscription, err := i.Datasource.FetchAsync(ctx, i.currentBlock.Height-1, -1, ch)
+	if err != nil {
+		return errors.Wrap(err, "failed to call fetch async")
+	}
 
-					if err := i.Processor.Process(ctx, blocks); err != nil {
-						return errors.WithStack(err)
-					}
-
-					// Update current state
-					i.currentBlock = blocks[len(blocks)-1].Header
-				case <-subscription.Done():
-					// end current loop
-					break
-				case <-ctx.Done():
-					subscription.Unsubscribe()
-					return errors.WithStack(ctx.Err())
-				case err := <-subscription.Err():
-					if err != nil {
-						return errors.Wrap(err, "got error while fetch async")
-					}
-				}
-			}
-
-			// Prepare range of blocks to sync
-			startHeight, endHeight, skip, err := i.prepareRange(i.currentBlock.Height)
-			if err != nil {
-				return errors.Wrap(err, "failed to prepare range")
-			}
-			if skip {
+	for {
+		select {
+		case blocks := <-ch:
+			// empty blocks
+			if len(blocks) == 0 {
 				continue
 			}
 
-			logger.InfoContext(ctx, "Syncing blocks from %d to %d\n", startHeight, endHeight)
-
-			// TODO: supports chain reorganization
-
-			/*
-				Concurrent block processing (fetch + process)
-				1. Fetch blocks concurrently
-				2. Process blocks sequentially
-			*/
-
-			blockHash, err := i.btcclient.GetBlockHash(endHeight)
-			if err != nil {
-				return errors.Wrap(err, "failed to get block hash")
-			}
-
-			if endHeight <= i.currentBlock.Height && !blockHash.IsEqual(&i.currentBlock.Hash) {
-				// TODO: Chain reorganization detected, need to re-index
-				log.Println("Chain reorganization detected, need to re-index")
-			}
-
-			block, err := i.btcclient.GetBlock(blockHash)
-			if err != nil {
-				return errors.Wrap(err, "failed to get block")
-			}
-
-			b := types.ParseMsgBlock(block, endHeight)
-			if err := i.Processor.Process(ctx, []*types.Block{b}); err != nil {
+			if err := i.Processor.Process(ctx, blocks); err != nil {
 				return errors.WithStack(err)
 			}
-			i.currentBlock = b.Header
+
+			// Update current state
+			i.currentBlock = blocks[len(blocks)-1].Header
+		case <-subscription.Done():
+			// end current loop
+			return nil
+		case <-ctx.Done():
+			subscription.Unsubscribe()
+			return errors.WithStack(ctx.Err())
+		case err := <-subscription.Err():
+			if err != nil {
+				return errors.Wrap(err, "got error while fetch async")
+			}
 		}
 	}
 }
