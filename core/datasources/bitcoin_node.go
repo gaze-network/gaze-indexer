8a7b3b1675207e39141e870809da38a1ea3a41d4
Gaze <dev@gaze.network>
2024-04-15 00:40:16 +0700

    feat: implement bitcoin.Contract in bitcoin node datasource

--- core/datasources/bitcoin_node.go
+++ core/datasources/bitcoin_node.go
@@ -3,6 +3,7 @@ package datasources
 import (
 	"context"
 
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/cockroachdb/errors"
 	"github.com/gaze-network/indexer-network/core/types"
@@ -204,4 +205,14 @@ func (d *BitcoinNodeDatasource) prepareRange(fromHeight, toHeight int64) (start,
 	}
 
 	return start, end, false, nil
+}
+
+// GetTransaction fetch transaction from Bitcoin node
+func (d *BitcoinNodeDatasource) GetTransaction(ctx context.Context, txHash chainhash.Hash) (*types.Transaction, error) {
+	rawTx, err := d.btcclient.GetRawTransaction(&txHash)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get raw transaction")
+	}
+	msgTx := rawTx.MsgTx()
+	return types.ParseMsgTx(msgTx, 0, chainhash.Hash{}, 0), nil
 }
