733c04fce9bfb7f619515add3bac8215fcc6e200
Gaze <dev@gaze.network>
2024-04-16 23:33:22 +0700

    fix: rename postgres config key

--- modules/runes/config/config.go
+++ modules/runes/config/config.go
@@ -5,5 +5,5 @@ import "github.com/gaze-network/indexer-network/internal/postgres"
 type Config struct {
 	Datasource string          `mapstructure:"datasource"` // Datasource to fetch bitcoin data for Meta-Protocol e.g. `bitcoin-node` | `database`
 	Database   string          `mapstructure:"database"`   // Database to store runes data.
-	Postgres   postgres.Config `mapstructure:"postgresql"`
+	Postgres   postgres.Config `mapstructure:"postgres"`
 }
