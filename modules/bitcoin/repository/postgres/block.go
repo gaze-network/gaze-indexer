f3363313fb4cd86e4f00faa97f012af668cc4bbc
Gaze <gazenw@users.noreply.github.com>
2024-04-17 18:25:55 +0700

    fix: invalid pgx version
    Co-authored-by: Gaze <dev@gaze.network>

--- modules/bitcoin/repository/postgres/block.go
+++ modules/bitcoin/repository/postgres/block.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gaze-network/indexer-network/common/errs"
 	"github.com/gaze-network/indexer-network/core/types"
 	"github.com/gaze-network/indexer-network/modules/bitcoin/repository/postgres/gen"
-	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/v5"
 	"github.com/samber/lo"
 )
 
--- modules/bitcoin/repository/postgres/transaction.go
+++ modules/bitcoin/repository/postgres/transaction.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cockroachdb/errors"
 	"github.com/gaze-network/indexer-network/common/errs"
 	"github.com/gaze-network/indexer-network/core/types"
-	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/v5"
 )
 
 func (r *Repository) GetTransactionByHash(ctx context.Context, txHash chainhash.Hash) (*types.Transaction, error) {
