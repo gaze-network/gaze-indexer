fa581bbabc119925bcc7c8db70e39d394db73998
Gaze <gazenw@users.noreply.github.com>
2024-04-17 17:46:49 +0700

    fix(btc): remove uniqte index for hash
    Co-authored-by: Gaze <dev@gaze.network>

--- modules/bitcoin/database/postgresql/migrations/000001_initialize_table.up.sql
+++ modules/bitcoin/database/postgresql/migrations/000001_initialize_table.up.sql
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS "bitcoin_blocks" (
 	"nonce" INT NOT NULL
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS bitcoin_blocks_block_hash_idx ON "bitcoin_blocks" USING HASH ("block_hash");
+CREATE INDEX IF NOT EXISTS bitcoin_blocks_block_hash_idx ON "bitcoin_blocks" USING HASH ("block_hash");
 
 CREATE TABLE IF NOT EXISTS "bitcoin_transactions" (
 	"tx_hash" TEXT NOT NULL PRIMARY KEY,
