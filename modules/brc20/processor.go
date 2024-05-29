dc44e4bb5cd9e42172f5909cd042355bcecabc06
Gaze <dev@gaze.network>
2024-05-29 17:29:05 +0700

    Revert "feat: seed brc20 stats"
    This reverts commit 4ae169218f8d744f346d766e03a06f2a1149557a.
    
    # Conflicts:
    #	modules/brc20/constants.go

--- modules/brc20/processor.go
+++ modules/brc20/processor.go
@@ -110,10 +110,10 @@ func (p *Processor) VerifyStates(ctx context.Context) error {
 			return errors.Wrap(err, "failed to count cursed inscriptions")
 		}
 		stats = &entity.ProcessorStats{
-			BlockHeight:             uint64(startingBlockData[p.network].Height),
-			CursedInscriptionCount:  startingBlockData[p.network].Stats.CursedInscriptionCount,
-			BlessedInscriptionCount: startingBlockData[p.network].Stats.BlessedInscriptionCount,
-			LostSats:                startingBlockData[p.network].Stats.LostSats,
+			BlockHeight:             uint64(startingBlockHeader[p.network].Height),
+			CursedInscriptionCount:  0,
+			BlessedInscriptionCount: 0,
+			LostSats:                0,
 		}
 	}
 	p.cursedInscriptionCount = stats.CursedInscriptionCount
@@ -127,10 +127,7 @@ func (p *Processor) CurrentBlock(ctx context.Context) (types.BlockHeader, error)
 	blockHeader, err := p.brc20Dg.GetLatestBlock(ctx)
 	if err != nil {
 		if errors.Is(err, errs.NotFound) {
-			return types.BlockHeader{
-				Height: startingBlockData[p.network].Height,
-				Hash:   startingBlockData[p.network].Hash,
-			}, nil
+			return startingBlockHeader[p.network], nil
 		}
 		return types.BlockHeader{}, errors.Wrap(err, "failed to get latest block")
 	}
