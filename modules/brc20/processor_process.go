fc48ba50b8f31ae22a9dbdbea3414ab18d69d773
Gaze <dev@gaze.network>
2024-05-31 20:46:08 +0700

    fix: remove panic

--- modules/brc20/processor_process.go
+++ modules/brc20/processor_process.go
@@ -42,10 +42,7 @@ func (p *Processor) Process(ctx context.Context, blocks []*types.Block) error {
 			return int(t1.NewSatPoint.Offset) - int(t2.NewSatPoint.Offset)
 		})
 
-		// process brc20 states
-		if err := p.processBRC20States(ctx, p.newInscriptionTransfers); err != nil {
-			return errors.Wrap(err, "failed to process brc20 states")
-		}
+		// TODO: process brc20 states
 
 		if err := p.flushBlock(ctx, block.Header); err != nil {
 			return errors.Wrap(err, "failed to flush block")
