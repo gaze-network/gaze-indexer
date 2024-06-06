033dbf73241ea5b72a0b80f577e387fb47043e8b
Gaze <dev@gaze.network>
2024-06-06 12:55:43 +0700

    fix: inscription transfers primary key add tx_index

--- modules/brc20/database/postgresql/migrations/000001_initialize_tables.up.sql
+++ modules/brc20/database/postgresql/migrations/000001_initialize_tables.up.sql
@@ -147,7 +147,7 @@ CREATE TABLE IF NOT EXISTS "brc20_inscription_transfers" (
 	"new_pkscript" TEXT NOT NULL,
 	"new_output_value" BIGINT NOT NULL,
 	"sent_as_fee" BOOLEAN NOT NULL,
-	PRIMARY KEY ("inscription_id", "block_height")
+	PRIMARY KEY ("inscription_id", "block_height", "tx_index")
 );
 CREATE INDEX IF NOT EXISTS brc20_inscription_transfers_block_height_tx_index_idx ON "brc20_inscription_transfers" USING BTREE ("block_height", "tx_index");
 CREATE INDEX IF NOT EXISTS brc20_inscription_transfers_new_satpoint_idx ON "brc20_inscription_transfers" USING BTREE ("new_satpoint_tx_hash", "new_satpoint_out_idx", "new_satpoint_offset");
