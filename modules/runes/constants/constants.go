3e7c5be52c095ff2bc5b19e92016d6bf6fdaf79e
Gaze <dev@gaze.network>
2024-10-05 01:21:34 +0700

    fix: remove starting block hash

--- modules/runes/constants/constants.go
+++ modules/runes/constants/constants.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/Cleverse/go-utilities/utils"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/gaze-network/indexer-network/common"
 	"github.com/gaze-network/indexer-network/core/types"
@@ -24,19 +23,15 @@ const (
 var StartingBlockHeader = map[common.Network]types.BlockHeader{
 	common.NetworkMainnet: {
 		Height: 839999,
-		Hash:   *utils.Must(chainhash.NewHashFromStr("0000000000000000000172014ba58d66455762add0512355ad651207918494ab")),
 	},
 	common.NetworkTestnet: {
 		Height: 2519999,
-		Hash:   *utils.Must(chainhash.NewHashFromStr("000000000006f45c16402f05d9075db49d3571cf5273cf4cbeaa2aa295f7c833")),
 	},
 	common.NetworkFractalMainnet: {
 		Height: 83999,
-		Hash:   *utils.Must(chainhash.NewHashFromStr("0000000000000000000000000000000000000000000000000000000000000000")), // TODO: Update this to match real hash
 	},
 	common.NetworkFractalTestnet: {
 		Height: 83999,
-		Hash:   *utils.Must(chainhash.NewHashFromStr("00000000000000613ddfbdd1778b17cea3818febcbbf82762eafaa9461038343")),
 	},
 }
 
